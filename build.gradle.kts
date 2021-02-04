b703fe008292bdde2506ea452553110f26cea73d
Andrea <andrea.giordano5@studio.unibo.it>
2021-02-04 18:01:51 +0100

    added task to compute version into readme

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,6 +63,17 @@ tasks.register("generateVersionFile") {
     File("$buildDir/version").writeText(version.toString())
 }
 
+tasks.register("computeVersion") {
+    val reg = Regex("Version: \\[([0-9]+\\.){2}[0-9]+(-[0-9a-zA-Z-+]*)?]")
+    val readme = File("$projectDir/README.md")
+    val text = readme.readText()
+    if (reg.containsMatchIn(text)) {
+        readme.writeText(text.replace(reg, "Version: [$version]"))
+    } else {
+        readme.appendText("Version: [$version]\n")
+    }
+}
+
 application {
     // Define the main class for the application
     mainClass.set("faustoCoppiLabAutomation.MainClassKt")
